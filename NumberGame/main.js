07db8fa1ce26e108d5cad995d420826dc8ec2cdd
victoryalhj <victoryalhj@gmail.com>
2024-07-29 23:01:33 +0100

    first commit

---
+++ NumberGame/main.js
@@ -0,0 +1,9 @@
+//Assign a random number
+//User put number and click go button
+//If the user guesses the number correctly, correct!
+//random number < user number Down!
+//random number > user number Up!
+//Click the reset Button, Reset!
+//The game ends when all 5 chances are used(Button disable)
+//If the user enters a number outside the range of 1~100, they informed. Not counted
+//If the user enters a number has already been guessed, informed. Not counted
